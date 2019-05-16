d18002817d997e3bfac5e014124780bc52f04050
Regan Ryan <regan.ryan.nz@gmail.com>
2019-05-16 17:09:04 +0200

    added scrolling button for navbar

---
+++ app/assets/javascripts/animatedScrollLeft.js
@@ -0,0 +1,44 @@
+document.addEventListener("turbolinks:load", function() {
+
+    document.getElementById('scrollLeft').onclick = function () {
+        if (document.body.scrollLeft == 0) {
+            scrollLeft(document.body, 240, 400);
+        } else {
+            scrollLeft(document.body, 0, 400);
+        }
+    }
+
+    function scrollLeft(element, to, duration) {
+    var start = element.scrollLeft,
+        change = to - start,
+        currentTime = 0,
+        increment = 20;
+
+    var animateScroll = function(){
+        currentTime += increment;
+        var val = Math.easeInOutQuad(currentTime, start, change, duration);
+        element.scrollLeft = val;
+        if(currentTime < duration) {
+            setTimeout(animateScroll, increment);
+        }
+    };
+    animateScroll();
+    }
+
+    //t = current time
+    //b = start value
+    //c = change in value
+    //d = duration
+    Math.easeInOutQuad = function (t, b, c, d) {
+    t /= d/2;
+    if (t < 1) return c/2*t*t + b;
+    t--;
+    return -c/2 * (t*(t-2) - 1) + b;
+    };
+
+
+
+    // Scroll from nav to content on page load
+    scrollLeft(document.body, 240, 400);
+    history.scrollRestoration = 'manual';
+});
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -19,6 +19,4 @@
 // This scrolls the url address bar out of the way on mobile devices, simulating a fullscreen app
 // It also scrolls the navigation out of the way (so the user starts on content, but has a glimpse
 // of where the navigation is)
-document.addEventListener("turbolinks:load", function() {
-  window.scrollTo(240,0);
-});
+
