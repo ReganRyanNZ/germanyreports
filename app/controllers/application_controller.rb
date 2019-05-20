3d61e3a9100d2bebac0e5603635e384bde22e87f
Regan Ryan <regan.ryan.nz@gmail.com>
2019-05-20 18:19:24 +0200

    added tübingen and sorted navbar

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,15 +3,21 @@ class ApplicationController < ActionController::Base
 
   # This setup is very un-rails-y, not quote sure where the logic should go..
   # The basic premise is that app/views/reports/categories/**/* is where any
-  # files or folders added will automatically populate the navbar.
+  # files added will automatically populate the navbar.
   def setup_nav_items
     dir = Rails.root.join('app', 'views', 'reports', 'categories').to_s
     @nav_items = {}
-    cats = Dir.glob("#{dir}/*")
+    cats = %W(
+      the_churches
+      developing_localities
+      cities_we_are_burdened_for
+      various_works
+      building_projects
+    )
     cats.each do |cat|
-      @nav_items[cat.split("/").last] = Dir.glob("#{cat}/*").map do |path|
+      @nav_items[cat] = Dir.glob("#{dir}/#{cat}/*").map do |path|
         path.remove(".slim").split("/").last
-      end
+      end.sort
     end
     @current_page = request.env['PATH_INFO']
   end
