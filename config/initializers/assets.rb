4fa346219781dfff4bca8839f18b923fc1ca5757
Regan Ryan <regan.ryan.nz@gmail.com>
2019-06-15 15:09:04 +0200

    added zurich report

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -4,7 +4,12 @@
 Rails.application.config.assets.version = '1.0'
 
 # Add additional assets to the asset load path.
+Dir.glob("#{Rails.root}/app/assets/images/**/").each do |path|
+  Rails.application.config.assets.paths << path
+end
 # Rails.application.config.assets.paths << Emoji.images_path
+
+
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
 
