44af4ea087774857669a724cee7bf66583c4a46a
Regan <regan@crewmeister.com>
2019-10-21 10:55:32 +0200

    added basic idea for archiving old reports

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -5,4 +5,13 @@ class ReportsController < ApplicationController
   def show
     render "reports/categories/#{params[:category_id]}/#{params[:report_id]}"
   end
+
+  def archives
+    # put some nav system, grouped by date
+    @archive_links = { "July_2019" => ["bielefeld"] }
+  end
+
+  def show_archive
+    render "reports/archives/#{params[:date_id]}/#{params[:archive_id]}"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,6 @@ Rails.application.routes.draw do
   resources :reports, except: [:show]
   get ':category_id/:report_id' => 'reports#show'
   root 'reports#index'
+  get 'archives' => 'reports#archives'
+  get 'archives/:date_id/:archive_id' => 'reports#show_archive'
 end
